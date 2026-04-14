47a1e876bdfacc08605c2eed835ca7ff42ada340
ermin-mumic <ermin.mumic@uzh.ch>
2026-04-14 14:46:45 +0200

    implement flat-RAM data streaming via DuckDB and Python generators for SF 100 compatibility

--- data_loader.py
+++ data_loader.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import duckdb
+import os
+import math
 
 
 JOIN_KEY_RENAME_MAP = {
@@ -35,46 +38,72 @@ JOIN_KEY_RENAME_MAP = {
     },
 }
 
-def prepare_data(table_names: list, csv_dir: str = '.', batch_size: int = 1) -> list:
-
-    tables = {}
-
-    print("Loading CSV files.")
-    for table_name in table_names:
-        csv_file = f"{csv_dir}/{table_name.lower()}.csv"
-        df = pd.read_csv(csv_file)
-        df.columns = df.columns.str.upper()
-
-        rename_map = JOIN_KEY_RENAME_MAP.get(table_name.upper(), {})
-        if rename_map:
-            df = df.rename(columns=rename_map)
-
-        tables[table_name] = df
-        print(f" {table_name}: {len(df)} rows")
+def prepare_data_generator(table_names: list, csv_dir: str = '.', batch_size: int = 1):
     
-    insert_sequence = []
+    table_names = [t.upper() for t in table_names]
+    con = duckdb.connect(database=":memory:")
+    total_rows_to_be_inserted = 0
+    row_counts = {}
 
-    current_row = {name: 0 for name in table_names}
+    print("Mapping and Renaming CSV files.\n")
+    for table_name in table_names:
+        csv_file = os.path.join(csv_dir, f"{table_name.lower()}.csv")
 
-    while True:
-        added_any = False
+        table_renames = JOIN_KEY_RENAME_MAP.get(table_name, {})
+        columns_info = con.execute(f"DESCRIBE SELECT * FROM read_csv_auto('{csv_file}')").fetchall()
 
+        select_items = []
+        for col in columns_info:
+            old_name = col[0]
+            old_name_upper = old_name.upper()
+
+            new_name = table_renames.get(old_name_upper, old_name_upper)
+
+            if "DATE" in old_name_upper:
+                sql_col = f'CAST ("{old_name}" AS VARCHAR) AS {new_name}'
+            else:
+                sql_col = f'"{old_name}" AS {new_name}'
+
+            select_items.append(sql_col)
+        
+        select_clause = ", ".join(select_items)
+
+        con.execute(f"""
+            CREATE VIEW {table_name}_raw AS 
+            SELECT {select_clause}
+            FROM read_csv_auto('{csv_file}')  
+        """)
+
+        count = con.execute(f"SELECT count(*) FROM {table_name}_raw").fetchone()[0]
+        row_counts[table_name] = count
+        total_rows_to_be_inserted += count
+
+        print(f" {table_name}: {count} rows mapped.")
+
+    offsets = {t: 0 for t in table_names}
+
+    yield {"total_rows": total_rows_to_be_inserted, "total_batches": math.ceil(total_rows_to_be_inserted/batch_size)}
+
+    while any(offsets[t]<row_counts[t] for t in table_names):
         for table_name in table_names:
-            df = tables[table_name]
-            start = current_row[table_name]
+            start = offsets[table_name]
+            total = row_counts[table_name]
 
-            if start >= len(df):
+            if start >= total:
                 continue
 
-            end = min(start + batch_size, len(df))
-            batch_df = df.iloc[start:end]
+            query = f"SELECT * FROM {table_name}_raw LIMIT {batch_size} OFFSET {start}"
+            batch_df = con.execute(query).df()
 
-            # Convert batch dataframe to JSON array of objects representing rows --> '[{"col1":1, "col2":"a"},{"col1":2, "col2":"b"}]
-            insert_sequence.append((table_name, batch_df.to_json(orient="records", date_format="epoch"))) 
-            current_row[table_name] = end
-            added_any = True
-        
-        if not added_any:
-            break
-                
-    return insert_sequence
+            # Convert batch dataframe to JSON array of objects representing rows
+            # --> '[{"col1":1, "col2":"a"},{"col1":2, "col2":"b"}]' as feldera requires json_flavor = pandas
+            json_str = batch_df.to_json(orient="records")
+
+            yield (table_name, json_str)
+
+            offsets[table_name] += batch_size
+
+
+    con.close()
+
+            
--- experiment.py
+++ experiment.py
@@ -7,7 +7,7 @@ from feldera.pipeline import Pipeline
 from feldera.enums import PipelineStatus
 from feldera.rest.pipeline import Pipeline as RestPipeline
 
-from data_loader import prepare_data
+from data_loader import prepare_data_generator
 
 
 class Experiment:
@@ -52,17 +52,14 @@ class Experiment:
         if self.pipeline is not None:
             self.pipeline.stop(force=True)
 
-    def _stats(self):
-        return self.pipeline.stats().global_metrics
-
     def _snapshot(self):
-        stats = self._stats()
+        stats = self.pipeline.stats().global_metrics
         return {
             "uptime_msecs": stats.uptime_msecs or 0,
             "total_completed_records": stats.total_completed_records or 0,
             "rss_mib": (stats.rss_bytes or 0) / (1024 * 1024),
         }
-
+    #TODO: remove this as required looping through whole insert sequence instead let data loader compute that while preparing data
     @staticmethod
     def _count_input_rows(insert_sequence):
         return int(sum(len(json.loads(batch_serialized)) for _, batch_serialized in insert_sequence))
@@ -85,10 +82,15 @@ class Experiment:
             json.dump(payload, f, indent=2)
         print(f"Results saved to {filename}")
 
-    def run_trial(self, trial_id, insert_sequence):
+    def run_trial(self, trial_id, insert_generator):
         print(f"\n{'#' * 70}")
         print(f"TRIAL {trial_id}/{self.trials}")
         print(f"{'#' * 70}\n")
+
+        # Executes DuckDB mapping
+        metadata = next(insert_generator)
+        print(f"\nTotal rows in all tables: {metadata['total_rows']}")
+        print(f"Amount of batches to be inserted: {metadata['total_batches']}\n")
 
         self._start_pipeline()
         memory_timeline = []
@@ -101,7 +103,8 @@ class Experiment:
             }
         )
 
-        for table_name, batch_serialized in insert_sequence:
+        # Generate data batches
+        for table_name, batch_serialized in insert_generator:
             self.client.push_to_pipeline(
                 self.pipeline_name,
                 table_name,
@@ -139,8 +142,8 @@ class Experiment:
             "trial": trial_id,
             "pipeline_name": self.pipeline_name,
             "batch_size": self.batch_size,
-            "input_batches_submitted": len(insert_sequence),
-            "input_rows_submitted": self._count_input_rows(insert_sequence),
+            "input_rows_submitted": metadata["total_rows"],
+            "input_batches_submitted": metadata["total_batches"],
             "throughput_rows_per_sec": throughput_rows_per_sec,
             "start_uptime_msecs": start_snap["uptime_msecs"],
             "end_uptime_msecs": end_snap["uptime_msecs"],
@@ -158,10 +161,14 @@ class Experiment:
         print(f"Peak memory: {peak_memory_mib:.2f} MiB")
 
 
-    def run_all(self, insert_sequence):
-        print(f"\nPrepared {len(insert_sequence)} batch inserts")
+    def run_all(self, table_names, csv_dir, batch_size):
         for trial in range(1, self.trials + 1):
-            self.run_trial(trial, insert_sequence)
+            insert_generator = prepare_data_generator(
+                table_names=table_names,
+                csv_dir=csv_dir,
+                batch_size=batch_size
+            )
+            self.run_trial(trial, insert_generator)
 
 
 def parse_args():
@@ -204,11 +211,6 @@ if __name__ == "__main__":
     print(f"Trials: {args.trials}")
     print(f"Tables: {', '.join(table_names)}")
 
-    insert_sequence = prepare_data(
-        table_names=table_names,
-        csv_dir=args.csv_dir,
-        batch_size=args.batch_size,
-    )
 
     experiment = Experiment(
         pipeline_name=args.pipeline_name,
@@ -218,7 +220,11 @@ if __name__ == "__main__":
         output_dir=args.output_dir,
         sf=os.path.basename(args.csv_dir)
     )
-    experiment.run_all(insert_sequence)
+    experiment.run_all(
+        table_names=table_names,
+        csv_dir=args.csv_dir,
+        batch_size=args.batch_size
+    )
 
     print("\n" + "=" * 70)
     print("EXPERIMENT COMPLETE")
